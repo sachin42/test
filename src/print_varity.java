f5eb97aaf378b021af4236eaaa99a3239f890786
Sachin Kushawaha <54734235+sachin42@users.noreply.github.com>
2022-12-01 17:10:10 +0530

    clear

--- src/print_varity.java
+++
@@ -1,7 +0,0 @@
-public class print_varity {
-    public static void main(String[] args) {
-        int a = 12;
-        System.out.println("a is = "+a);
-        System.out.printf("a is = %d", a);
-    }
-}
